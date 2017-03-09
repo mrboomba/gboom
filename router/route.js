fa1d98a61ddfb24274db51e16df1bc4bf11ad78e
Mr.Boomba <mrboomba161@gmail.com>
2017-03-09 19:30:33 +0700

    add static path for image

--- router/route.js
+++ router/route.js
@@ -13,4 +13,44 @@ router.get('/home',function(req,res){
 
 })
 
+
+//path for images
+router.get('/img/cat',function(req,res){
+	res.sendFile(path.resolve('img/cat.jpg'));
+
+})
+router.get('/img/dog1',function(req,res){
+	res.sendFile(path.resolve('img/dog1.jpg'));
+
+})
+router.get('/img/dog2',function(req,res){
+	res.sendFile(path.resolve('img/dog2.jpg'));
+
+})
+router.get('/img/dog3',function(req,res){
+	res.sendFile(path.resolve('img/dog3.jpg'));
+
+})
+router.get('/img/dog4',function(req,res){
+	res.sendFile(path.resolve('img/dog4.jpg'));
+
+})
+router.get('/img/dog5',function(req,res){
+	res.sendFile(path.resolve('img/dog5.jpg'));
+
+})
+router.get('/img/dog6',function(req,res){
+	res.sendFile(path.resolve('img/cat.jpg'));
+
+})
+router.get('/img/dog7',function(req,res){
+	res.sendFile(path.resolve('img/dog7.jpg'));
+
+})
+router.get('/img/dog8',function(req,res){
+	res.sendFile(path.resolve('img/dog8.jpg'));
+
+})
+
+
 module.exports = router;
