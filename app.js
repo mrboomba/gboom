11e08befc6354a86a7c5353770c372ca35a8e7e1
Mr.Boomba <mrboomba161@gmail.com>
2017-03-10 23:16:56 +0700

    add path to style

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use('/',route);
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-
+app.use('/style', express.static(__dirname + '/view')); // redirect CSS bootstrap
 
 app.listen(port, function () {
   console.log('Meow coming at port 8080!')
