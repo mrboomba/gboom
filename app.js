38e53c69548587c8b363b40dc2d34556906c2871
Mr.Boomba <mrboomba161@gmail.com>
2017-03-11 02:45:06 +0700

    build api

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var configDB = require('./config/database.js');
 var route    = require('./router/route');
+var api 	 = require('./api/api');
+var score    = require('./model/highscore');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 mongoose.connect(configDB.url); // connect to our database
 app.use('/',route);
@@ -11,7 +17,15 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); /
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/style', express.static(__dirname + '/view')); // redirect CSS bootstrap
+app.use('/api',api);
+
+
 
 app.listen(port, function () {
-  console.log('Meow coming at port 8080!')
+  score.remove({},function(err){
+  	if(err){return err}
+  		else  console.log('Meow coming at port 8080!')
+  })
+
+ 
 })
